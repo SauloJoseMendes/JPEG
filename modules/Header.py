a4c8697b58786fcba0f664cf481624fd59af52d1
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2024-03-03 22:26:16 +0000

    fix header quality factor

--- modules/Header.py
+++ modules/Header.py
@@ -3,10 +3,10 @@ import numpy as np
 from scipy.fft import dct
 
 class Header:
-    def __init__(self, downsampling_rate=422, interpolation=cv2.INTER_LINEAR, block_size=None):
+    def __init__(self, downsampling_rate=422, interpolation=cv2.INTER_LINEAR, block_size=None, quality_factor = 75):
         self.downsampling_rate = downsampling_rate
         self.interpolation = interpolation
         self.block_size = block_size
         self.rows = 0 
         self.columns = 0
-        self.quality_factor = 75
+        self.quality_factor = quality_factor
